8945edbbe09af6c253e6e66bffc1ac57dcba5058
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-04-10 13:18:04 +0530

    Add files via upload

---
+++ User/AddUser.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Card from '../UI/Card';
+
+const AddUser = (props) =>{
+
+    const addUserHandler = (event) =>{
+        event.preventDefault ();
+    };
+
+     return (
+         <Card>
+            <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>UserName</label>
+        <input  id ='username' type='text' />
+        <label htmlFor='age'>Age</label>
+        <input  id ='age' type='number' />
+        <button type='submit'>Add User</button>
+     </form> 
+         </Card>
+     )
+};
+
+export default AddUser;
