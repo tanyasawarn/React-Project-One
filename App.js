96fead9b19eb91fe959c95de6e55eea4e49f7820
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-04-10 13:15:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AddUser from './component/User/AddUser';
+ 
+function App (){
+  return (
+    <div>
+    <AddUser />
+      </div>
+  )
+  
+}
+ 
+export default App;
